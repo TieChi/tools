401998d511722413629cf2198d70e64006ad83ca
hanjun <654777646@qq.com>
2019-05-07 20:47:14 +0800

    m

---
+++ remove@.js
@@ -0,0 +1,32 @@
+let fs = require('fs')
+let path = require('path')
+
+
+// 获取命令行参数
+let parm = process.argv.splice(2)
+// 第一个参数是路径
+let rootPath = parm[0]
+// 后面的所有参数都是文件后缀
+let types = parm.splice(1)
+
+async function line(path) {
+    let files = fs.readdirSync(path)
+    files.forEach( name => {
+			if (!name.startsWith('.')) {
+				let oldPath = name;
+				let newPath = oldPath.split('@').join('');
+				if (oldPath !== newPath) {
+					fs.rename(path + '/' + oldPath, path + '/' + newPath, function(err) {
+							if (err) {
+									throw err;
+							}
+					});
+				}
+			}
+		})
+}
+
+
+;(async () => {
+    await line(rootPath)
+})()
